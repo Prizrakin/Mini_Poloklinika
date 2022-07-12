ed76f1cebbc51db9aa12f207e7a4a3a27878b08f
Prizrakin <prizrakin.king@gmail.com>
2022-07-12 13:53:09 +0300

    FIANL

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :recommendations
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
+  ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
